ab99dce40c27a88e811f1608ac9348c93d832bc4
Chao Liu <liuchao8989@gmail.com>
2016-01-18 12:35:09 -0800

    fix checkbox error

--- src/list-item.jsx
+++ src/list-item.jsx
@@ -3,21 +3,20 @@ var Firebase = require('firebase');
 var rootUrl ='https://sizzling-inferno-5872.firebaseio.com/';
 module.exports = React.createClass({
   getInitialState: function(){
-    var checked = this.props.item.done ? "checked" : ""
     return {
       text: this.props.item.text,
-      checked: checked
+      done: this.props.item.done
     }
   },
-  componemtnWillmount: function(){
-    this.fb = new Firebase(rootUrl + 'items/'+ this.props.item.key);
+  componentWillMount: function(){
+    this.fb = new Firebase(rootUrl + 'items/' + this.props.item.key);
   },
   render: function(){
     return <div className="input-group">
       <span className="input-group-addon">
         <input
         type="checkbox"
-        checked={this.state.checked}
+        checked={this.props.item.done}
         onChange={this.handleDoneChange}
         />
       </span>
@@ -31,53 +30,9 @@ module.exports = React.createClass({
       </span>
     </div>
   },
-  handleDoneChange: function(event){
-    console.log("this is handleDoneChange");
-    
-    this.setState({checked: ""})
-    // this.fb.update({done: false})
+  handleDoneChange: function(event) {
+    var update = {done: event.target.checked}
+    this.setState(update);
+    this.fb.update(update);
   }
-
-})
-
-// var React = require('react');
-// var Firebase = require('firebase');
-// var rootUrl = 'https://blistering-torch-4253.firebaseio.com/';
-
-// module.exports = React.createClass({
-//   getInitialState: function() {
-//     return {
-//       text: this.props.item.text,
-//       done: this.props.item.done
-//     }
-//   },
-//   componentWillMount: function() {
-//     this.fb = new Firebase(rootUrl + 'items/' + this.props.item.key);
-//   },
-//   render: function() {
-//     return <div className="input-group">
-//       <span className="input-group-addon">
-//         <input
-//           type="checkbox"
-//           onChange={this.handleDoneChange}
-//           checked= ""//{this.props.item.done? "checked" : ""}
-//           />
-//       </span>
-//       <input type="text"
-//         className="form-control"
-//         value={this.state.text}
-//         />
-//       <span className="input-group-btn">
-//         <button className="btn btn-default">
-//           Delete
-//         </button>
-//       </span>
-//     </div>
-//   },
-//   handleDoneChange: function(event) {
-//     console.log("onChange");
-//     var update = {done: true}
-//     this.setState(update);
-//     this.fb.update(update);
-//   }
-// });
+});
